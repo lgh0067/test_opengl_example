1847bd747ffba843c7872d6001e2995e5c550a83
이기호 <lgh0067@naver.com>
2021-05-03 15:09:31 +0900

    week_09
    week_09

--- src/context.h
+++ src/context.h
@@ -21,6 +21,7 @@ public:
 private:
     Context() {}
     bool Init();
+    ProgramUPtr m_simpleProgram;
     ProgramUPtr m_program;
 
     VertexLayoutUPtr m_vertexLayout;
@@ -29,8 +30,36 @@ private:
     TextureUPtr m_texture;
     TextureUPtr m_texture2;
     	
+    // animation
+    bool m_animation { true };
+
     // clear color
     glm::vec4 m_clearColor { glm::vec4(0.1f, 0.2f, 0.3f, 0.0f) };
+
+    // light parameter
+    struct Light {
+        glm::vec3 position { glm::vec3(3.0f, 3.0f, 3.0f) };
+        glm::vec3 ambient { glm::vec3(0.1f, 0.1f, 0.1f) };
+        glm::vec3 diffuse { glm::vec3(0.5f, 0.5f, 0.5f) };
+        glm::vec3 specular { glm::vec3(1.0f, 1.0f, 1.0f) };
+    };
+    Light m_light;
+
+    // material parameter
+    struct Material {
+        TextureUPtr diffuse;
+        TextureUPtr specular;
+        float shininess { 32.0f };
+    };
+    Material m_material;
+
+    // light parameter	
+    glm::vec3 m_lightPos { glm::vec3(3.0f, 3.0f, 3.0f) };
+    glm::vec3 m_lightColor { glm::vec3(1.0f, 1.0f, 1.0f) };
+    glm::vec3 m_objectColor { glm::vec3(1.0f, 0.5f, 0.0f) };
+    float m_ambientStrength { 0.1f };
+    float m_specularStrength { 0.5f };
+    float m_specularShininess { 32.0f };
 
     bool m_cameraControl { false };
     glm::vec2 m_prevMousePos { glm::vec2(0.0f) };
