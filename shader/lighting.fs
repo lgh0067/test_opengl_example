1457f92303fdf60d0cf32166b42a1d5927e9ef4e
이기호 <lgh0067@naver.com>
2021-05-31 19:06:13 +0900

    week_13
    week_13

--- shader/lighting.fs
+++ shader/lighting.fs
@@ -23,6 +23,7 @@ struct Material {
     float shininess;
 };
 uniform Material material;
+uniform int blinn;
 
 void main() {
     vec3 texColor = texture2D(material.diffuse, texCoord).xyz;
@@ -46,9 +47,17 @@ void main() {
         vec3 diffuse = diff * texColor * light.diffuse;
 
         vec3 specColor = texture2D(material.specular, texCoord).xyz;
+        float spec = 0.0;
+    if (blinn == 0) {
         vec3 viewDir = normalize(viewPos - position);
         vec3 reflectDir = reflect(-lightDir, pixelNorm);
-        float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+        spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+        }
+    else {
+        vec3 viewDir = normalize(viewPos - position);
+        vec3 halfDir = normalize(lightDir + viewDir);
+        spec = pow(max(dot(halfDir, pixelNorm), 0.0), material.shininess);
+        }
         vec3 specular = spec * specColor * light.specular;
 
     result += (diffuse + specular) * intensity;
